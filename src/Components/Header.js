ef4173c34f82578a22f076394cabb56df0c37d8d
Diogo Bento <diogo.bento@bringglobal.com>
2023-06-30 01:07:32 +0000

    feat: Header and Footer.

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header className="border-b p-3 flex justify-between items-center">
+            <span className="font-bold">
+                News Express
+            </span>
+
+        </header>
+    )
+}
+
+export default Header
